517995a25d4659c7039cf98cfc4f8c2901a7f4f1
glassesneo <glassesneo@protonmail.com>
2025-03-06 13:13:20 +0900

    fix: export plugins

--- src/ryurei.nim
+++ src/ryurei.nim
@@ -2,6 +2,8 @@ import pkg/rulecs
 import pkg/vmath
 import pkg/sokol/app as sokol_app
 import ryurei/core/[application, builtin, plugin, types]
+import ryurei/plugins/time/time
+import ryurei/plugins/graphics/graphics
 
 export rulecs, vmath, application, builtin, plugin, types
 export sokol_app.Keycode, sokol_app.Mousebutton
